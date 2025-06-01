e4b2da4532d461719bd2ceb151ce6aa6a066b443
Koi to Coco <koitococo@outlook.com>
2025-06-02 00:04:30 +0800

    feat: move util func from mxa to common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -6,6 +6,7 @@ pub mod protocol;
 pub mod system_info;
 pub mod utils;
 pub mod script;
+pub mod util_func;
 
 pub const VERSION: &str = concat!(env!("CARGO_PKG_VERSION"), " - ", env!("GIT_HASH"));
 
---
+++ common/src/util_func/mod.rs
@@ -0,0 +1,75 @@
+use std::{hash::Hasher, process::Stdio};
+
+use anyhow::Result;
+use futures_util::StreamExt;
+use log::{error, info};
+use tokio::{fs::File, io::AsyncWriteExt, process::Command};
+use xxhash_rust::xxh3::Xxh3;
+
+/// Download a file from the given URL and save it to the given path. Return the xxh3 hash of the file.
+pub async fn download_file(url: &str, path: &str) -> Result<String> {
+  info!("Downloading file from {url} to {path}");
+  let response = reqwest::get(url).await?;
+  if response.status().is_success() {
+    let mut out = File::create(path).await?;
+    let mut body = response.bytes_stream();
+    let mut hasher = Xxh3::new();
+    while let Some(chunk) = body.next().await {
+      let chunk = chunk?;
+      hasher.write(&chunk);
+      out.write_all(&chunk).await?;
+    }
+    let hash = format!("{:x}", hasher.finish());
+    info!("Downloaded file from {url} to {path}. xxh3: {hash}");
+    Ok(hash)
+  } else {
+    error!("Failed to download file from {url}. Server returned an error.");
+    anyhow::bail!("Failed to download file from {}", url);
+  }
+}
+
+/// Upload a file to the given URL.
+pub async fn upload_file(url: &str, path: &str) -> Result<()> {
+  info!("Uploading file from {path} to {url}");
+  if reqwest::Client::new().put(url).body(File::open(path).await?).send().await?.status().is_success() {
+    Ok(())
+  } else {
+    error!("Failed to upload file to {url}. Server returned an error.");
+    anyhow::bail!("Failed to upload file to {}", url);
+  }
+}
+
+/// Execute an external command and return its output.
+pub async fn execute_command(cmd: &String, args: Vec<String>) -> Result<(i32, String, String)> {
+  info!("Executing external command: {cmd} {args:?}");
+  let child = Command::new(cmd)
+    .args(args)
+    .stdin(Stdio::null())
+    .stdout(Stdio::piped())
+    .stderr(Stdio::piped())
+    .spawn()?;
+  let output = child.wait_with_output().await?;
+  Ok((
+    output.status.code().unwrap_or(-1),
+    String::from_utf8(output.stdout)?,
+    String::from_utf8(output.stderr)?,
+  ))
+}
+
+#[inline]
+async fn execute_script(cmd: &String) -> Result<(i32, String, String)> {
+  info!("Executing script: {cmd}");
+  const TMP_SCRIPT_PATH: &str = "/tmp/mxa-script.sh";
+  let mut file = File::create(TMP_SCRIPT_PATH).await?;
+  file.write_all(cmd.as_bytes()).await?;
+  file.flush().await?;
+  execute_command(&("sh".to_string()), vec![TMP_SCRIPT_PATH.to_string()]).await
+}
+
+pub async fn execute_shell(cmd: &String, use_script_file: bool) -> Result<(i32, String, String)> {
+  if use_script_file {
+    execute_script(cmd).await
+  } else {
+    execute_command(&("sh".to_string()), vec!["-c".to_string(), cmd.to_string()]).await
+  }
+}
